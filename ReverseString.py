da413f20de98296d290deaa9ff1f46a75a4c9bf6
Lucas Vahle <56959974+Lucas42313456@users.noreply.github.com>
2021-02-16 13:02:32 +0100

    Update ReverseString.py

--- ReverseString.py
+++ ReverseString.py
@@ -3,16 +3,16 @@ print(len(String))
 for x in String:
   print(x)
 
-Gnirts= ""
-Gnirts+="!"
-print(Gnirts)
+gnirtS= ""
+gnirtS+="!"
+print(gnirtS)
 
 #len(String)-1 is last letter
 #String[0] is first letter
 #loop on letters from first to last letter via: for x in range(len(String))
-#empty string is Gnirts
+#empty string is gnirtS
 
 for x in range(len(String)):
-  Gnirts+=String[len(String)-1-x]
+  gnirtS+=String[len(String)-1-x]
 
-print(Gnirts)                                         
+print(gnirtS)                                         
